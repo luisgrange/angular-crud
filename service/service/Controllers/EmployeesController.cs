7bfb922e24a841d93764053b23707902c40c8694
luisgrange <luisfernando237grange@outlook.com>
2023-03-15 14:18:56 -0300

    Add the back-end

---
+++ service/service/Controllers/EmployeesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using service.Data;
+using service.Models;
+
+namespace service.Controllers
+{
+    [ApiController]
+    [Route("/api/employees")]
+    public class EmployeesController : Controller
+    {
+        private readonly ServiceDbContext _dbContext;
+        public EmployeesController(ServiceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _dbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+            await _dbContext.Employees.AddAsync(employeeRequest);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(employeeRequest);
+
+        }
+
+
+    }
+}
---
+++ service/service/Data/ServiceDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using service.Models;
+
+namespace service.Data
+{
+    public class ServiceDbContext : DbContext
+    {
+        public ServiceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
