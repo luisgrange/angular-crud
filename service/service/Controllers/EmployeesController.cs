e866a60824ed430117869e4c4098680fa00a6bc3
luisgrange <luisfernando237grange@outlook.com>
2023-03-31 14:50:13 -0300

    Add Edit and Delete routes

--- service/service/Controllers/EmployeesController.cs
+++ service/service/Controllers/EmployeesController.cs
@@ -34,6 +34,62 @@ namespace service.Controllers
 
         }
 
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+            var employee = await _dbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmpployee([FromRoute] Guid id, Employee updateEmployeeRequest)
+        {
+            var employee = await _dbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Department = updateEmployeeRequest.Department;
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee ([FromRoute] Guid id)
+        {
+            var employee = await _dbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Employees.Attach(employee);
+            _dbContext.Employees.Remove(employee);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
+
+
+
+        }
 
     }
 }
