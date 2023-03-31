998fd84128880d266b6f385b39e7e0938e55e437
luisgrange <luisfernando237grange@outlook.com>
2023-03-31 14:52:24 -0300

    Create Edit page

--- client/src/app/components/employees/edit-employee/edit-employee.component.ts
+++ client/src/app/components/employees/edit-employee/edit-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IEmployee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-edit-employee',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-employee.component.css']
 })
 export class EditEmployeeComponent {
+  employeeDetails: IEmployee = {
+    id: '',
+    name: '',
+    email: '',
+    phone: 0,
+    salary: 0,
+    department: ''
+  }
+  constructor (private route: ActivatedRoute, private employeeService: EmployeesService, private router: Router){}
 
+  ngOnInit(): void{
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const id = params.get('id');
+
+        if(id){
+          //call the API here
+          this.employeeService.getEmployee(id).subscribe({
+            next: (response) => {
+              this.employeeDetails = response;
+            }
+          })
+        }
+      }
+    })
+  }
+
+  updateEmployee() {
+    this.employeeService.updateEmployee(this.employeeDetails?.id, this.employeeDetails).subscribe({
+      next: (response) => {
+        this.router.navigate(['/']);
+      }
+    })
+  }
+
+  deleteEmployee(id: string){
+    this.employeeService.deleteEmployee(id).subscribe({
+      next: (response) => {
+        this.router.navigate(['/']);
+      }
+    })
+  }
 }
