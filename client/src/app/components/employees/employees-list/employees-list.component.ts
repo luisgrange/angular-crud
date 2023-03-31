a8148e1d21f89809bf757deafd80f3b240969713
luisgrange <luisfernando237grange@outlook.com>
2023-03-31 14:52:01 -0300

    Create Delete function on employees list

--- client/src/app/components/employees/employees-list/employees-list.component.ts
+++ client/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IEmployee } from '../../../models/employee.model';
 import { EmployeesService } from 'src/app/services/employees.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employees-list',
@@ -12,12 +13,20 @@ export class EmployeesListComponent {
   employees: IEmployee[] = [];
 
 
-  constructor(private employeesService: EmployeesService) {}
+  constructor(private employeesService: EmployeesService, private router: Router) {}
 
   ngOnInit(): void{
     this.employeesService.getAllEmployees().subscribe({
       next: (employees) => {this.employees = employees},
       error: (response) => {console.log(response)}
     });
-  } 
+  }
+
+  deleteEmployee(id: string){
+    this.employeesService.deleteEmployee(id).subscribe({
+      next: (response) => {
+        this.router.navigate(['/']);
+      }
+    })
+  }
 }
